52ffdce5efabdb4f9112657c4033f35dd1f5d514
Claudia Saimbert <csaimbert@gmail.com>
2021-06-14 22:57:19 -0400

    :seedling: : Added seed file for basic user model

---
+++ script/seed.js
@@ -0,0 +1,17 @@
+const db = require('../server/db');
+const { User } = require('../server/db/models');
+
+const seedUser = [
+  {
+    name: 'ClaudiaTheFirst',
+    email: 'claudiathee@gmail.com',
+  },
+  {
+    name: 'AriTheMagnificent',
+    email: 'arithee@gmail.com',
+  },
+  {
+    name: 'SabrinaTheSecond',
+    email: 'sabrinathee@gmail.com',
+  },
+];
