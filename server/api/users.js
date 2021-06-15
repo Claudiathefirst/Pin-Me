df299fbdd1488c642f8a9f99a2a3861b512814b4
Claudia Saimbert <csaimbert@gmail.com>
2021-06-14 22:54:44 -0400

    :sparkles: created wuth folder

--- server/api/users.js
+++ server/api/users.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
+const { User } = require('../db/models');
 
 router.get('/', async (req, res, next) => {
   try {
-  } catch (err) {}
+    const users = await User.findAll({
+      attributes: ['email', 'id'],
+    });
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
