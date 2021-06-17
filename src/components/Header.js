3717762be7c055b968501daab6e319dae54931e7
Claudia Saimbert <csaimbert@gmail.com>
2021-06-16 23:31:18 -0400

    :sparkles: : started building header component

---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+import PinterestIcon from '@material-ui/icons/Pinterest';
+import { IconButton } from '@material-ui/core';
+
+// Styled Components
+// TRouBLe
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 56px;
+  padding: 12px 4px 4px 16px;
+  background-color: white;
+  color: black;
+`;
+
+// styling pinterest button in logo wrapper
+const LogoWrapper = styled.div`
+  .MuiSvgIcon-root {
+	  color: #c60023
+    font-size: 32px;
+	cursor: pointer;
+
+  }
+`;
+const HomePageButton = styled.button``;
+// const FollowingButton = styled.button``;
+// const SearchWrapper = styled.div``;
+// const SearchBarWrapper = styled.div``;
+
+function Header() {
+  return (
+    <Wrapper>
+      <LogoWrapper>
+        <IconButton>
+          <PinterestIcon />
+        </IconButton>
+      </LogoWrapper>
+      <HomePageButton>
+        <a href='/'>Homepage</a>
+      </HomePageButton>
+
+      {/* <FollowingButton>
+        <a href='/'>Following</a>
+      </FollowingButton>
+      <SearchWrapper>
+        <SearchBarWrapper></SearchBarWrapper>
+      </SearchWrapper> */}
+    </Wrapper>
+  );
+}
+
+export default Header;
