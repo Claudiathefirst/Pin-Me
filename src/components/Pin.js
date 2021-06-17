c5e1feafdeabf1e24f0bde23fe14e6fc236be217
Claudia Saimbert <csaimbert@gmail.com>
2021-06-17 13:13:01 -0400

    :sparkles: : single pin

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -1,6 +1,34 @@
-// import React from 'react';
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+import styled from 'styled-components';
 
-// function Pin() {
-//   return <div></div>;
-// }
-// export default Pin;
+function Pin() {
+  return (
+    <Wrapper>
+      <Container>
+        <img src='https://images.unsplash.com/photo-1534806391029-791d2695c38b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJhYmllc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60 ' />
+      </Container>
+    </Wrapper>
+  );
+}
+export default Pin;
+
+const Wrapper = styled.div`
+  display: inline-flex;
+  padding: 8px;
+`;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  cursor: pointer;
+  width: 236px;
+  img {
+    display: flex;
+    width: 100%;
+    cursor: zoom-in;
+    border-radius: 16px;
+    object-fit: cover;
+  }
+`;
+// object fit so image is scaled properly
