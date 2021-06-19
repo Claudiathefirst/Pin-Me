734f46e378f175b78db7a8e1d76019cf60a89c5d
Claudia Saimbert <csaimbert@gmail.com>
2021-06-19 13:19:26 -0400

    :fire: : removed hard coded data

--- src/components/Mainboard.js
+++ src/components/Mainboard.js
@@ -1,8 +1,38 @@
-// import React from 'react';
-// import Pin from './Pin.js';
+import React from 'react';
+import styled from 'styled-components';
+import Pin from './Pin.js';
 
-// function Mainboard() {
-//   return <div></div>;
-// }
+function Mainboard(props) {
+  console.log(props);
+  const { pins } = props;
 
-// export default Mainboard;
+  return (
+    <div>
+      <Wrapper>
+        <Container>
+          {pins.map((pins, idx) => {
+            return <Pin key={idx} url={pins} />;
+          })}
+        </Container>
+      </Wrapper>
+    </div>
+  );
+}
+
+export default Mainboard;
+
+//Styled Components
+
+const Wrapper = styled.div`
+  display: flex;
+  background-color: white;
+  justify-content: center;
+  margin-top: 15px;
+  height: 100%;
+  width: 100%;
+`;
+const Container = styled.div`
+  display: flex;
+  background-color: white;
+  width: 80%;
+`;
